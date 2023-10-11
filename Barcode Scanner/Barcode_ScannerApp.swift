3ce00e641bc00a73e3d605ac172c7c2536f175ff
ankitvat <ankitvat874@gmail.com>
2023-10-11 13:57:47 +0530

    Initial Commit

---
+++ Barcode Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode Scanner
+//
+//  Created by Ankit Vat on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
